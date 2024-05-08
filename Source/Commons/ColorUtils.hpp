2773296ee460263761725c65dafea018fe28f9a2
Subnite <vanrietilias@hotmail.com>
2024-05-08 19:50:21 +0200

    should have all the functions to approximate color hz now.

---
+++ Source/Commons/ColorUtils.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include "juce_core/juce_core.h"
+#include "juce_graphics/juce_graphics.h"
+#include <vector>
+
+namespace HueShift{
+
+class ColorInfo {
+public:
+	float frequency;
+	juce::Range<float> hue;
+	ColorInfo(float frequency, juce::Range<float> hue)
+	: frequency(frequency), hue(hue)
+	{}
+
+	static ColorInfo GetClosestColor(juce::Colour c){
+		auto hue = c.getHue(); // [0:1]
+
+		auto colors = GetColors();
+		for (auto& color : colors) {
+			if (color.hue.contains(hue)) return color;
+		}
+
+		// if not found then it's higher than pink aka red
+		return red;
+	}
+
+	static std::vector<ColorInfo> GetColors() {
+		return std::vector<ColorInfo>({red, orange, yellow, green, cyan, blue, violet, pink});
+	}
+
+	static const ColorInfo red;
+	static const ColorInfo orange;
+	static const ColorInfo yellow;
+	static const ColorInfo green;
+	static const ColorInfo cyan;
+	static const ColorInfo blue;
+	static const ColorInfo violet;
+	static const ColorInfo pink;
+};
+
+const ColorInfo ColorInfo::red 		= 	ColorInfo(4.5f, 			juce::Range<float>(0.000f, 0.042f));
+const ColorInfo ColorInfo::orange 	= 	ColorInfo(5.f, 				juce::Range<float>(0.042f, 0.111f));
+const ColorInfo ColorInfo::yellow 	= 	ColorInfo(5.166666666f, 	juce::Range<float>(0.111f, 0.172f));
+const ColorInfo ColorInfo::green 	= 	ColorInfo(5.5f, 			juce::Range<float>(0.172f, 0.431f));
+const ColorInfo ColorInfo::cyan 	= 	ColorInfo(6.0f, 			juce::Range<float>(0.431f, 0.494f));
+const ColorInfo ColorInfo::blue 	= 	ColorInfo(6.666666666f, 	juce::Range<float>(0.494f, 0.694f));
+const ColorInfo ColorInfo::violet 	= 	ColorInfo(7.5f, 			juce::Range<float>(0.694f, 0.806f));
+const ColorInfo ColorInfo::pink 	= 	ColorInfo(8.333333333f, 	juce::Range<float>(0.806f, 0.944f));
+}
